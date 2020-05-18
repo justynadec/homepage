274080e9e017078119040978e5fc30e25d4b5f5c
Justyna Dec <justynadec@o2.pl>
2020-05-18 22:50:02 +0200

    styles changes

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-const name='Justyna';
-const age=25;
-console.log(name);
-console.log(age);
-console.log(`Siema, nazywam się ${name} i mam ${age} lat.`);
+const name = 'Justyna';
+const age = '25';
+
+const heading = document.querySelector('.main__heading--js')
+
+heading.innerHTML = `Witaj, nazywam się ${name} i mam ${age} lat.`;
+
+const emptyParagraph = document.querySelector('.week-summary__description--js')
+
+emptyParagraph.innerHTML = `Uzupełniłam treść javascriptem!`;
+
+
+   const greet = (age, name) => {
+       console.log(
+        `Witaj drogi odwiedzający, nazwyam się ${name} i mam ${age} lat`
+       );
+   }
+
+greet(25, 'Justyna');
+
+function createContent (querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+}
+
+createContent('.week-summary__description--js', 'Siemka');
